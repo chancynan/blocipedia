3628a773c7efc2025b11ee6c950faee7bd8862a6
Nancy <nc.nancy.chan@gmail.com>
2015-10-12 19:38:42 -0700

    Project in progress

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,21 @@
+class WikisController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
