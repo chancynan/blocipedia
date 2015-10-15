b65c85663c1f13e848f652af33d36704927c8adc
Nancy <nc.nancy.chan@gmail.com>
2015-10-15 05:45:28 -0700

    Started adding CRUD and views

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,18 +4,42 @@ class WikisController < ApplicationController
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
   end
 
   def create
-
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body, :private))
+    if @wiki.save
+      redirect_to @wiki, notice: "Wiki was saved successfully."
+    else
+      flash[:error] = "Error creating topic. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body, :private))
+      redirect_to @wiki
+    else
+      flash[:error] = "Error saving wiki, please try again."
+      render :edit
+    end
+  end
+
+  def delete
+    @wiki = Wiki.find(params[:id])
   end
 
   def destroy
+    @wiki = Wiki.find(params[:id]).destroy
   end
 end
